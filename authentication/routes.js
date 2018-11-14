939e66c4265ee2f6a9863408f0a4018c9da747af
SerksesE <serkses@hotmail.com>
2018-11-14 15:25:25 +0100

    .

--- authentication/routes.js
+++ authentication/routes.js
@@ -1,73 +1,21 @@
 const { Router } = require('express')
-
 const router = new Router()
+const { toJWT, toData } = require('./jwt')
 
-router.get('/customers', (req, res, next) => {
-  Customer
-    .findAll()
-    .then(customers => {
-      res.send({ customers })
-    })
-    .catch(error => next(error))
-})
+router.post('/logins', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
 
-router.get('/customers/:id', (req, res, next) => {
-  Customer
-    .findById(req.params.id, {include: [Company]})
-    .then(customer => {
-      if (!customer) {
-        return res.status(404).send({
-          message: `Customer does not exist`
-        })
-      }
-      return res.send(customer)
+  if (!email || !password) {
+    res.status(400).send({
+      message: 'Please supply a valid email and password'
     })
-    .catch(error => next(error))
-})
-
-router.post('/logins', (req, res, next) => {
-  Customer
-    .create(req.body)
-    .then(customer => {
-      if (!customer) {
-        return res.status(404).send({
-          message: `Customer does not exist`
-        })
-      }
-      return res.status(201).send(customer)
+  }
+  else {
+    res.send({
+      jwt: toJWT({ userId: 1 })
     })
-    .catch(error => next(error))
-})
-
-router.put('/customers/:id', (req, res, next) => {
-  Customer
-    .findById(req.params.id)
-    .then(customer => {
-      if (!customer) {
-        return res.status(404).send({
-          message: `Customer does not exist`
-        })
-      }
-      return customer.update(req.body).then(customer => res.send(customer))
-    })
-    .catch(error => next(error))
-})
-
-router.delete('/customers/:id', (req, res, next) => {
-  Customer
-    .findById(req.params.id)
-    .then(customer => {
-      if (!customer) {
-        return res.status(404).send({
-          message: `Customer does not exist`
-        })
-      }
-      return customer.destroy()
-        .then(() => res.send({
-          message: `Customer was deleted`
-        }))
-    })
-    .catch(error => next(error))
-})
+  }
+}
 
 module.exports = router

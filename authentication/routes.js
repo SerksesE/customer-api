764d466ff179bada5555e30baf175ac33f366637
SerksesE <serkses@hotmail.com>
2018-11-14 15:59:52 +0100

    secret endpoint

--- authentication/routes.js
+++ authentication/routes.js
@@ -18,4 +18,21 @@ router.post('/logins', (req, res) => {
   }
 })
 
+router.get('/secret-endpoint', (req, res) => {
+  const auth = req.headers.authorization && req.headers.authorization.split(' ')
+  
+  if (auth && auth[0] === 'Bearer' && auth[1]) {
+    const data = toData(auth[1])
+    res.send({
+      message: 'Thanks for visiting the secret endpoint.',
+      data
+    })
+  }
+  else {
+    res.status(401).send({
+      message: 'Please supply some valid credentials'
+    })
+  }
+})
+
 module.exports = router
